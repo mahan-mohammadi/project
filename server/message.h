d7d6ef11be766a98103139820b187f434e08aa3c
mahan-mohammadi <mahan.mohammadi.a@gmail.com>
2025-08-06 00:24:39 +0330

    message blueprint

---
+++ server/message.h
@@ -0,0 +1,22 @@
+#include <ctime>
+#include <string>
+
+using namespace std;
+
+class Message{
+    public:
+    //TODO: add a json constructor and a to json?????
+    Message();
+    Message(int senderId, int reciverId, string content);
+    // so we can load from the db without updating the timestamp
+    Message(int senderId, int reciverId, string content, time_t time);
+    int getSenderId();
+    int getReciverId();
+    string getMessage();
+    time_t getTime();
+    private:
+    int senderId;
+    int reciverId;
+    string content;
+    time_t time;
+};
