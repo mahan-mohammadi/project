58a01fd912d32e5149f966d45ddaaa3f28bcf1b1
mahan-mohammadi <mahan.mohammadi.a@gmail.com>
2025-08-06 12:43:19 +0330

    implemented message class

---
+++ server/message.cpp
@@ -0,0 +1,48 @@
+#include "message.h"
+
+Message::Message(){
+    senderId = 0;
+    reciverId = 0;
+    message = "";
+    timestamp = time(nullptr);
+}
+
+Message::Message(int senderId , int reciverId , string message){
+    this->senderId = senderId;
+    this->reciverId = reciverId;
+    this->message = message;
+    timestamp = time(nullptr);
+}
+
+Message::Message(json j){
+    this->senderId = j.at("senderId");
+    this->reciverId = j.at("reciverId");
+    this->message = j.at("message");
+    timestamp = j.at("timestamp");
+}
+
+int Message::getSenderId() {
+    return this->senderId;
+}
+
+int Message::getReciverId() {
+    return this->reciverId;
+}
+
+string Message::getMessage() {
+    return this->message;
+}
+
+time_t Message::getTime() {
+    return this->timestamp;
+}
+
+json Message::toJson(){
+    json j;
+    j["senderId"] = this->senderId;
+    j["reciverId"] = this->reciverId;
+    j["message"] = this->message;
+    j["timestamp"] = this->timestamp;
+
+    return j;
+}
--- server/message.h
+++ server/message.h
@@ -7,11 +7,9 @@ using json = nlohmann::json;
 
 class Message{
     public:
-    //TODO: add a json constructor and a to json????? also add to and from json
     Message();
     Message(int, int, string );
     // so we can load from the db without updating the timestamp
-    Message(int, int , string , time_t );
     Message(json);
     int getSenderId();
     int getReciverId();
@@ -22,5 +20,5 @@ class Message{
     int senderId;
     int reciverId;
     string message;
-    time_t time;
+    time_t timestamp;
 };
