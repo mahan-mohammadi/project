cd3adda863642c37dd59665421398eb3f6c236fb
mahan-mohammadi <mahan.mohammadi.a@gmail.com>
2025-08-06 12:28:32 +0330

    fixed message.h

--- server/message.h
+++ server/message.h
@@ -3,21 +3,24 @@
 #include "json.hpp"
 
 using namespace std;
+using json = nlohmann::json;
 
 class Message{
     public:
     //TODO: add a json constructor and a to json????? also add to and from json
     Message();
-    Message(int senderId, int reciverId, string content);
+    Message(int, int, string );
     // so we can load from the db without updating the timestamp
-    Message(int senderId, int reciverId, string content, time_t time);
+    Message(int, int , string , time_t );
+    Message(json);
     int getSenderId();
     int getReciverId();
     string getMessage();
     time_t getTime();
+    json toJson();
     private:
     int senderId;
     int reciverId;
-    string content;
+    string message;
     time_t time;
 };
